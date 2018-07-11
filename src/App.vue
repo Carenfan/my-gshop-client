7d661e12df4dacdc739c953a4b75d3fb2c2c22f7
Caren fan <646112407@qq.com>
2018-07-11 17:28:49 +0800

    commit1

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuide v-show="$route.meta.isShow"/>
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+  export default {
+
+    mounted () {
+      this.$store.dispatch('getAddress')
+      this.$store.dispatch('getUser')
+    },
+    created () {
+        // 异步获取address
+        this.$store.dispatch('getAddress')
+      // 异步获取登陆用户信息
+        this.$store.dispatch('getUserInfo')
+    },
+
+    components: {
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
---
+++ src/store/mutations.js
@@ -0,0 +1,42 @@
+import {
+  RECEIVE_ADDRESS,
+  RECEIVE_CATEGORYS,
+  RECEIVE_SHOPS,
+  RECEIVE_USER,
+  RESET_USER
+} from './mutation-types'
+
+export default {
+  [RECEIVE_ADDRESS] (state, {address}) {
+    state.address = address
+  },
+
+  [RECEIVE_CATEGORYS] (state, {categorys}) {
+    state.categorys = categorys
+  },
+
+  [RECEIVE_SHOPS] (state, {shops}) {
+    state.shops = shops
+  },
+
+  [RECEIVE_USER] (state, {user}) {
+    state.user = user
+  },
+  [RESET_USER] (state) {
+    state.user = { }
+  },
+  [RECEIVE_INFO](state, {info}) {
+    state.info = info
+  },
+
+  [RECEIVE_RATINGS](state, {ratings}) {
+    state.ratings = ratings
+  },
+
+  [RECEIVE_GOODS](state, {goods}) {
+    state.goods = goods
+  },
+  [RECEIVE_USER_INFO](state, {userInfo}) {
+    state.userInfo = userInfo
+  },
+}
